6850e27033d11614847b195c0a99e72193be342c
acoosto <stojaal1@students.zhaw.ch>
2023-05-18 22:23:08 +0200

    changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello DevOps Course :-)');
+    res.send('Hello DevOps Course :-) - prove that it worked in Jenkins as well');
 });
 
 app.listen(PORT, HOST);
