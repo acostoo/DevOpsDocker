40678185d83c1d4356bde9c92ac26c3fca5eca21
acoosto <stojaal1@students.zhaw.ch>
2023-05-23 19:11:22 +0200

    changes

--- server.js
+++ server.js
@@ -8,9 +8,8 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello DevOps Course FS 2023 :-) - Aleksandar');
+    res.send('Hello DevOps Course :-))) - Aleksandar');
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on
-http://${HOST}:${PORT}`);
+console.log(`Running on http://${HOST}:${PORT}`);
